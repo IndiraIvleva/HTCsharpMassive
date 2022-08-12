10fc797aca91648780447da35ad424d808885b5a
Indira Ivleva <indira.ivleva@gmail.com>
2022-08-12 16:29:11 +0300

    add changes in HT2

--- HT2/Program.cs
+++ HT2/Program.cs
@@ -4,20 +4,19 @@
 78 -> третьей цифры нет
 32679 -> 6*/
 
-int n = new Random().Next(0, 10000);
+int n = new Random().Next(0, 100000);
 Console.WriteLine($"Задано число - {n}");
 int b = n;
 if (n < 100)
 {
     Console.WriteLine($"Третьей цифры числа {b} нет");
+
 }
 else
 {
     while (n > 1000)
     {
-        n = n / 10;     
+        n = n / 10;  
     }
-}
 Console.WriteLine($"Третья цифра числа {b} - {n % 10}");
-
-    
+}
